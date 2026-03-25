4573dfe6e8196230ae0a0a292ce69e0f3e112930
cuteribs <85863433+cuteribs@users.noreply.github.com>
2026-03-26 00:05:10 +0800

    update

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -22,7 +22,7 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const packageJson = require('../package.json');
 const packageVersion = packageJson.version;
-const mcpServerName = `Azure DevOps PR Helper MCP Server v${packageVersion}`;
+const mcpServerName = `Azure DevOps PR Helper MCP Server`;
 
 const argv = yargs(hideBin(process.argv))
   .scriptName("mcp-server-adopr-helper")
